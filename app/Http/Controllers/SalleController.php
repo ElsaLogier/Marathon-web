b991221cde6e7c44509a31f80489c19f7a9c0592
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-12-14 23:32:29 +0100

    fix mistake et ajout requete parametrée

--- app/Http/Controllers/SalleController.php
+++ app/Http/Controllers/SalleController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Oeuvre;
 use App\Models\Salle;
 use Illuminate\Http\Request;
 
 class SalleController extends Controller
 {
-    public function store(Request $request, int $id)
+    public function show(Request $request, int $id)
     {
+        $auteur = $request->get('auteur');
         $salle = Salle::findOrFail($id);
-        $oeuvres = $salle->oeuvres;
+        if (empty($auteur)) $oeuvres = $salle->oeuvres;
+        else $oeuvres = $salle->oeuvres()->where('auteur', $auteur)->get();
         $sallesSuivantes = $salle->suivantes;
         return view('salle', ['oeuvres' => $oeuvres, 'salle' => $salle, 'sallessuivantes' => $sallesSuivantes]);
     }
