c5ad1cd16ad72669971de80f9e17ee0af523d613
Arnaud Fievet <arnaud_fievet@ens.univ-artois.fr>
2022-12-15 15:18:11 +0100

    commit temporaire

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/scss/app.scss', 'resources/css/acc.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/scss/app.scss', 'resources/css/acc.css',
+                'resources/css/salle.css','resources/js/app.js'],
             refresh: true,
         }),
     ],
