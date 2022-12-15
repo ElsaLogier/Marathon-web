d1ca4403ad0b96872f2dcc61ab6320f1ec0de799
Elsa Logier <elsa.logier@gmail.com>
2022-12-15 16:48:52 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/login.css','resources/css/acc.css', 'resources/css/acc.css', 'resources/css/salle.css','resources/css/register.css','resources/scss/app.scss', 'resources/js/app.js','resources/js/burger.js','resources/scss/profil.css',],
+            input: ['resources/css/app.css', 'resources/css/login.css','resources/css/acc.css', 'resources/css/acc.css', 'resources/css/salle.css','resources/css/register.css','resources/scss/app.scss', 'resources/js/app.js','resources/js/burger.js','resources/css/oeuvre.css'],
             refresh: true,
         }),
     ],
